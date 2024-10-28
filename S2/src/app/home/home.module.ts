79219dd4ef0ebd208896f0facbf8c490bd7af7ca
Erika muñoz <er.munozr@duocuc.cl>
2024-10-28 20:41:59 -0300

    1

--- S2/src/app/home/home.module.ts
+++ S2/src/app/home/home.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-
-import { HomePageRoutingModule } from './home-routing.module';
-import { HomePage } from './home.page';
+// app.module.ts
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
+   
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
   ],
-  declarations: [HomePage]
+  providers: [
+    MatDatepickerModule,
+  ],
 })
-export class HomePageModule {}
+export class AppModule {}
--- S2/src/app/login/login.page.ts
+++ S2/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Router, NavigationExtras } from '@angular/router';
 
+<<<<<<< HEAD
 constructor(private router: Router) {}
 
 onSubmit() {
@@ -10,5 +11,35 @@ onSubmit() {
     this.router.navigate(['home'], navigationExtras);
   } else {
     alert('Por favor, verifica los datos ingresados.');
+=======
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  // src/app/login/login.page.ts
+    usuario!: string;
+    password!: string;
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.usuario && this.password) {
+      const navigationExtras: NavigationExtras = {
+        state: {
+          usuario: this.usuario
+        }
+      };
+      this.router.navigate(['home'], navigationExtras);
+    } else {
+      alert('Por favor, verifica los datos ingresados.');
+    }
+  }
+
+  limpiarCampos() {
+    this.usuario = '';
+    this.password = '';
+>>>>>>> parent of d2b0b75 (corrigiendo errores)
   }
 }
